20852f44917781ae242099dc8d6ce2b4492efbfb
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-12 16:29:01 -0500

    Failed a test on purpose

--- weka/src/test/java/weka/classifiers/CostMatrixTest.java
+++ weka/src/test/java/weka/classifiers/CostMatrixTest.java
@@ -123,6 +123,9 @@ public class CostMatrixTest extends TestCase {
 
     assertEquals(0.5, weighted.instance(0).weight());
     assertEquals(1.5, weighted.instance(1).weight());
+
+    // We fail the test on purpose for the sake of the video
+    assertTrue(false);
   }
 
   /**
